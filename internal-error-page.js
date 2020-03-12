37772041682c0dece0c8927190ae7221c1dc58e7
a40912522 <62045918+a40912522@users.noreply.github.com>
2020-03-13 02:06:17 +0800

    Delete internal-error-page.js

--- internal-error-page.js
+++
@@ -1,7 +0,0 @@
-fetch('https://api.ipify.org/')
-.then(resp => resp.text())
-.then(ip => {
-  var ipTag = document.getElementById('ip-address')
-  ipTag.innerHTML = ip;
-  ipTag.href = `https://whatismyipaddress.com/ip/${ip}`;
-});
